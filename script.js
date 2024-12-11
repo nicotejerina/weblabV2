8a86c48e091f9ea87b3c5e85ea077639c91b299c
Nicolás Tejerina <nicoteje97@gmail.com>
2024-12-11 13:06:17 -0300

    subi todo

---
+++ script.js
@@ -0,0 +1,9 @@
+// Función para mostrar/ocultar las páginas de un niño
+function togglePages(id) {
+    const element = document.getElementById(id);
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+}
